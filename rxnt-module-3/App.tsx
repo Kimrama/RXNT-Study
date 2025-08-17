60b32ff22179c2ef1a2687cd1400dcee7412bf78
Kimrama <kimrama0x@gmail.com>
2025-08-17 23:30:46 +0700

    nested navigation

--- rxnt-module-3/App.tsx
+++ rxnt-module-3/App.tsx
@@ -2,13 +2,30 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoriteScreen from "./screens/FavoriteScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
+function DrawerNavigator() {
+    return (
+        <Drawer.Navigator
+            screenOptions={{
+                headerStyle: { backgroundColor: "#351401" },
+                headerTintColor: "white",
+                sceneStyle: { backgroundColor: "#3f2f25" },
+            }}
+        >
+            <Drawer.Screen name="Categories" component={CategoriesScreen} />
+            <Drawer.Screen name="Favorites" component={FavoriteScreen} />
+        </Drawer.Navigator>
+    );
+}
 export default function App() {
     return (
         <>
@@ -23,10 +40,9 @@ export default function App() {
                 >
                     <Stack.Screen
                         name="Categories"
-                        component={CategoriesScreen}
+                        component={DrawerNavigator}
                         options={{
-                            headerShown: true,
-                            title: "Meal Categories",
+                            headerShown: false,
                         }}
                     />
                     <Stack.Screen
@@ -50,12 +66,3 @@ export default function App() {
         </>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
