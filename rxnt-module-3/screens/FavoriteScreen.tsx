c3faf28fa1326f98fe8e0237b1593c71def98b38
Kimrama <kimrama0x@gmail.com>
2025-09-06 09:13:01 +0700

    implement FavoriteScreen to display favorite meals using FavoritesContext

--- rxnt-module-3/screens/FavoriteScreen.tsx
+++ rxnt-module-3/screens/FavoriteScreen.tsx
@@ -1,11 +1,22 @@
 import { View, Text, StyleSheet } from "react-native";
-
+import MealsList from "../components/MealsList/MealsList";
+import { MEALS } from "../data/data";
+import { useContext } from "react";
+import { FavoritesContext } from "../store/context/favorites-context";
 function FavoriteScreen() {
-    return (
-        <View style={styles.container}>
-            <Text>Favorite Meals</Text>
-        </View>
+    const favoriteMealsCtx = useContext(FavoritesContext);
+    const favoriteMeals = MEALS.filter((meal) =>
+        favoriteMealsCtx.ids.includes(meal.id)
     );
+
+    if (favoriteMeals.length === 0) {
+        return (
+            <View style={styles.container}>
+                <Text>No favorite meals found!</Text>
+            </View>
+        );
+    }
+    return <MealsList items={favoriteMeals} />;
 }
 
 const styles = StyleSheet.create({
